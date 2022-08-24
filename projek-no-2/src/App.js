3df723e9272341c2fdd62583c0bb5833125a6423
Ajie Prana Mesa <109120170+AjiePM@users.noreply.github.com>
2022-08-24 09:29:36 +0800

    perbaiki

--- projek-no-2/src/App.js
+++ projek-no-2/src/App.js
@@ -7,28 +7,28 @@ function App() {
   return (
     <>
     <ul>
-      <li> <Link to ='/Artis/1'>Randi Pangalia</Link></li>
-      <li> <Link to ='/Artis/2'>Dedi Corbuser</Link></li>
-      <li> <Link to ='/Artis/3'>Inul Daratistas</Link></li>
-      <li> <Link to ='/Artis/4'>Sule</Link></li>
-      <li> <Link to ='/Artis/5'>Andre</Link></li>
-      <li> <Link to ='/Artis/6'>Udin</Link></li>
-      <li> <Link to ='/Artis/7'>Pesulap Merah</Link></li>
-      <li> <Link to ='/Artis/8'>Cupi Cupita</Link></li>
-      <li> <Link to ='/Artis/9'>Dewi Persik</Link></li>
-      <li> <Link to ='/Artis/10'>Doni</Link></li>
-      <li> <Link to ='/Artis/11'> Jaka Tingkir</Link></li>
-      <li> <Link to ='/Artis/12'>Fiki </Link></li>
-      <li> <Link to ='/Artis/13'>Daus Mini </Link></li>
-      <li> <Link to ='/Artis/14'>Dodi Sudrajad</Link></li>
-      <li> <Link to ='/Artis/15'>Demien</Link></li>
+      <li> <Link to ='/Artis/Randi_Pangalila'>Randi Pangalia</Link></li>
+      <li> <Link to ='/Artis/Dedi_Corbuser'>Dedi Corbuser</Link></li>
+      <li> <Link to ='/Artis/Inul_Daratista'>Inul Daratistas</Link></li>
+      <li> <Link to ='/Artis/sule'>Sule</Link></li>
+      <li> <Link to ='/Artis/Andre'>Andre</Link></li>
+      <li> <Link to ='/Artis/Udin'>Udin</Link></li>
+      <li> <Link to ='/Artis/Pesulap_merah'>Pesulap Merah</Link></li>
+      <li> <Link to ='/Artis/Cupi_Cupita'>Cupi Cupita</Link></li>
+      <li> <Link to ='/Artis/Dewi_persik'>Dewi Persik</Link></li>
+      <li> <Link to ='/Artis/Doni'>Doni</Link></li>
+      <li> <Link to ='/Artis/Jaka'> Jaka Tingkir</Link></li>
+      <li> <Link to ='/Artis/Fiki'>Fiki </Link></li>
+      <li> <Link to ='/Artis/Daus_Mini'>Daus Mini </Link></li>
+      <li> <Link to ='/Artis/Dodi_Sudrajad'>Dodi Sudrajad</Link></li>
+      <li> <Link to ='/Artis/Demien'>Demien</Link></li>
     </ul>
     <Routes>
-      <Route path = '/Artias/:nama_post'element ={<Post/>}/>
+      <Route path = '/Artis/:nama_artis'element ={<Post/>}/>
     </Routes>
     </>
    
-   
+
   );
 }
 
--- projek-no-2/src/Artis/Artis.jsx
+++ projek-no-2/src/Artis/Artis.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 
+
 const Artis= () =>{
 
     let nama_artis= useParams ()
 
     return (
         <>
-            Masuk ke Post {nama_artis}
+            {nama_artis.nama_artis}
         </>
     )
     
